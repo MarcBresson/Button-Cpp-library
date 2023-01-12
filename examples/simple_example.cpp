72da8f0fbde5489c30ad848c4cb2f054a74668b0
Marc Bresson <marco.bresson@gmail.com>
2023-01-12 21:34:43 +0000

    refactor: file rename and Serial printing
    Print the results for more clarity. The file has also been renamed to
    simple_example.

---
+++ examples/simple_example.cpp
@@ -0,0 +1,18 @@
+#include <Arduino.h>
+#include "button.h"
+
+Button button1;
+
+void setup(){
+
+}
+
+void loop(){
+    button1.update();
+
+    if(button1.onClick()){
+        Serial.println("Button clicked !");
+    } else if (button1.onHold(2000)){
+        Serial.println("Button held for more than 2s.");
+    }
+}
