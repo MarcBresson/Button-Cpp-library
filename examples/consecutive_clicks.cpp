16f9175461146e80b85bfb2a9057b4d5be3112df
Marc Bresson <marco.bresson@gmail.com>
2023-01-13 11:01:50 +0000

    fix: working consecutive clicks example

--- examples/consecutive_clicks.cpp
+++ examples/consecutive_clicks.cpp
@@ -2,6 +2,8 @@
 #include "button.h"
 
 Button button1;
+uint8_t previous_consecutive_clicks;
+uint8_t consecutive_clicks;
 
 void setup(){
 
@@ -9,11 +11,13 @@ void setup(){
 
 void loop(){
     button1.update();
-    uint8_t consecutive_clicks = button1.getNumberConsecutiveClicks(1000);
+    previous_consecutive_clicks = consecutive_clicks;
+    consecutive_clicks = button1.getNumberConsecutiveClicks(1000);
 
-    if(consecutive_clicks > 10){
+    if(previous_consecutive_clicks > 10 && consecutive_clicks == 0){
         Serial.println("The button has been clicked 10 times in a row.");
-    } else if (consecutive_clicks > 2){
+    } else
+    if (previous_consecutive_clicks > 2 && consecutive_clicks == 0){
         Serial.println("The button has been double clicked.");
     }
 }
